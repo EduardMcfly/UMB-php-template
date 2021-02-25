b9f5159ee10ee09db076a0ed8b100c94bf020c92
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-02-25 12:23:07 -0500

    chore: index.php added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/index.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php require_once('components/header.php'); ?>
+    <!-- Page Content -->
+    <div class="container mt-5">
+        <a href="./data.php">Carreras</a>
+    </div>
+    </div>
+    <?php require_once('components/footer.php'); ?>
+    <!-- Bootstrap core JavaScript -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
